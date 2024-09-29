dc36f9e55ec9eeab320e940e5f0642fe5ae7535c
KamalHazriq <kamalhazriq@gmail.com>
2024-09-29 11:41:55 +0800

    Initial Commit

---
+++ data/index.ts
@@ -0,0 +1,228 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Projects", link: "#projects" },
+    ././{ name: "Testimonials", link: "#testimonials" },
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "Eager to contribute my technical skills and learning to real-world projects ",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full absolute",
+      titleClassName: "justify-end",
+      img: "./b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "Ensuring seamless communication and collaboration",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My tech stack",
+      description: "I constantly try to learn and improve",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+      imgClassName: "",
+      titleClassName: "justify-center",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "./grid.svg",
+      spareImg: "./b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Currently looking for internship Opportunity",
+      description: "Current Focus",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "./b5.svg",
+      spareImg: "./grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "UKM EziParcel : Campus Parcel Management System",
+      des: "Developed a system to digitize parcel management process in campus.",
+      img: "./eziparcel.jpg",
+      iconLists: ["./html.svg", "./css.svg", "./javascript.svg", "./flutter.svg", "./dart.svg", "./firebase.svg"],
+      link: "https:././github.com./KamalHazriq./UKM-EziParcel-Web",
+    },
+    {
+      id: 2,
+      title: "Web Development : Product Catalog",
+      des: "Created a product catalog website for Disney products, learning the basis of CRUD and web development.",
+      img: "./disneyweb.jpg",
+      iconLists: ["./html.svg", "./css.svg", "./javascript.svg", "./php.svg", "./mysql.svg","./bootstrap.svg","./codeigniter.svg"],
+      link: "http:././lrgs.ftsm.ukm.my./users./a189646./enhancement./login.php",
+    },
+    {
+      id: 3,
+      title: "TP2543 Web Programming Deliverables",
+      des: "Website that showcases web programming modules I learned from web programming course in university.",
+      img: "./webprogramming.png",
+      iconLists: ["./html.svg", "./css.svg", "./javascript.svg", "./php.svg", "./mysql.svg","./bootstrap.svg","./codeigniter.svg"],
+      link: "http:././lrgs.ftsm.ukm.my./users./a189646./",
+    },
+    {
+      id: 4,
+      title: "Mobile Application Project : TickleTea",
+      des: "Mobile App Development Group Project that developed a teashop mobile app.",
+      img: "./tickletea.jpg",
+      iconLists: ["./java.svg", "./android.svg", "./figma.svg"],
+      link: "https:././youtu.be./p4FTguCQiO8",
+    },
+    {
+      id: 5,
+      title: "Human Computer Interaction Project: Digi World",
+      des: "Group Project to apply good user interface guidelines, principles, and theories.Designed and prototyped the UI./UX of a game application using Figma.",
+      img: "./digiworld.jpg",
+      iconLists: ["./figma.svg"],
+      link: "https:././www.figma.com./design./wLeEGzksNTAz37oMrXwXnA./DIGI-WORLD?node-id=0-1&t=6R7BtZ6cAKlqRrLO-1",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "cloudinary",
+      img: "./cloud.svg",
+      nameImg: "./cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "./app.svg",
+      nameImg: "./appName.svg",
+    },
+    {
+      id: 3,
+      name: "HOSTINGER",
+      img: "./host.svg",
+      nameImg: "./hostName.svg",
+    },
+    {
+      id: 4,
+      name: "stream",
+      img: "./s.svg",
+      nameImg: "./streamName.svg",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "./dock.svg",
+      nameImg: "./dockerName.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Frontend Engineer Intern",
+      desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+      className: "md:col-span-2",
+      thumbnail: "./exp1.svg",
+    },
+    {
+      id: 2,
+      title: "Mobile App Dev - JSM Tech",
+      desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+      className: "md:col-span-2", ././ change to md:col-span-2
+      thumbnail: "./exp2.svg",
+    },
+    {
+      id: 3,
+      title: "Freelance App Dev Project",
+      desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+      className: "md:col-span-2", ././ change to md:col-span-2
+      thumbnail: "./exp3.svg",
+    },
+    {
+      id: 4,
+      title: "Lead Frontend Developer",
+      desc: "Developed and maintained user-facing features using modern frontend technologies.",
+      className: "md:col-span-2",
+      thumbnail: "./exp4.svg",
+    },
+  ];
+  
+  export const socialMedia = [
+    {
+      id: '1',
+      img: "./git.svg",
+      link: "https:././github.com./KamalHazriq", 
+    },
+    {
+      id: '2',
+      img: "./email.svg",
+      link: "mailto:kamalhazriq@gmail.com", ././ Replace with your email
+    },
+    {
+      id: '3',
+      img: "./link.svg",
+      link: "https:././www.linkedin.com./in./kamalhazriq./", ././ Replace with your LinkedIn profile link
+    },
+  ];
